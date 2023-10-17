bbbd90ed8bb8a35bc33828bca0daf1b650a0e598
mgabrielo <121618872+mgabrielo@users.noreply.github.com>
2023-10-17 21:51:13 +0100

    Update client.js

--- React Native OTP + JWT authentication/auth-ui/app/api/client.js
+++ React Native OTP + JWT authentication/auth-ui/app/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create(
-    { baseUrl: 'http://192.168.1.167:8000/api/', }
+    { baseUrl: 'http://112.188.1.107:8000/api/', }
 )
 
 export default client;
